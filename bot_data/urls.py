4d2f6c17bfaeacb8dce086e7ce64ad85285596d4
Denis <denison99@mail.ru>
2024-02-14 19:33:18 +0500

    14 feb

--- bot_data/urls.py
+++ bot_data/urls.py
@@ -1,6 +1,6 @@
 dev_url = 'https://b24-idzi0o.bitrix24.ru/rest/1/2k2rrk0gfqyb4by2/'
-client_url = 'b24-eyazi6.bitrix24.ru/rest/13/jqxg8d9flw2scw2w/'
-result_url = dev_url
+client_url = 'https://b24-eyazi6.bitrix24.ru/rest/13/6tgnf491mw7k4ukk/'
+result_url = client_url
 urls = {
     'contact_add': result_url + 'crm.contact.add',
     'company_add': result_url + 'crm.company.add',
